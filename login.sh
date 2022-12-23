f0abcc0596dee74a3ffd26966e02e81a6849936d
Slimane <slimaneb2000@gmail.com>
2022-12-23 17:22:11 -0500

    read me

--- login.sh
+++
@@ -1,6 +0,0 @@
-echo "[default]
-aws_access_key_id=ASIA3R7FNQ4X6ZHPS44H
-aws_secret_access_key=uNCqQmNQsiQFxJ3Z5QxQ+LFG6vbRKHdNmfQLc/1+
-aws_session_token=FwoGZXIvYXdzEDgaDCHQPiPmVrHjCBaHbCLDAac+vRd04sg8bWfKrvJ3EWbbMHqp9zvDxz/PQqqjL+BQEliCp9er71Xyrkb80gCT0WPIQrvD+LXJiaCVDHy+BVp3rJDrFb2lPxuce3pYX3W2SCb/pM8UVRUAuqIEZ2/wqs2VyHlYTzoEECR8Ilp23WxwK6WSxy8QV5mp6A+M/UKxf/S+HHIbGI/LDOa5NUUcTrBTrUHl6ytaQz2zj96NKRLbAfMwCjD/PAHzsB5V+dTK6Zrq9WYC+ZV7r5Zkg4hqErKzhij5zMmcBjIthLgFLE26EW1ObEg5Kf6ux193/sCNa52XZpTrvXxi9oLRNfSwHjkpaWVspYzL
-" > ~/.aws/credentials
-
--- master.sh
+++ master.sh
@@ -7,33 +7,6 @@ sudo apt install -y libaio1 libmecab2
 sudo wget https://dev.mysql.com/get/Downloads/MySQL-Cluster-7.6/mysql-cluster-community-management-server_7.6.6-1ubuntu18.04_amd64.deb
 sudo dpkg -i mysql-cluster-community-management-server_7.6.6-1ubuntu18.04_amd64.deb
 
-#creating the config.ini files
-sudo mkdir /var/lib/mysql-cluster
-cd /var/lib/mysql-cluster/
-sudo touch config.ini
-sudo chmod 777 config.ini
-sudo cat <<< '
-[ndbd default]
-# Options affecting ndbd processes on all data nodes:
-NoOfReplicas=1	# Number of replicas
-
-[ndb_mgmd]
-# Management process options:
-hostname=172.31.29.189 # Hostname of the manager
-datadir=/var/lib/mysql-cluster 	# Directory for the log files
-
-[ndbd]
-hostname=172.31.31.40# Hostname/IP of the first data node
-NodeId=2			# Node ID for this data node
-datadir=/usr/local/mysql/data	# Remote directory for the data files
-
-[mysqld]
-# SQL node options:
-hostname=172.31.29.189
-' > config.ini
-
-cd /home/ubuntu/
-
 #downloads
 sudo wget https://dev.mysql.com/get/Downloads/MySQL-Cluster-7.6/mysql-cluster_7.6.6-1ubuntu18.04_amd64.deb-bundle.tar
 
